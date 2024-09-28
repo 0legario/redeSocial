75b5e28e8b71edab9f9064c3827c5ad346c16a6b
0legario <mgolegario.br@gmail.com>
2024-09-27 23:48:44 -0300

    Delete redeSocial directory

--- redeSocial/bd/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $banco = "redesocial";
-    $usuario = "root";
-    $senha = "root";
-    $porta = "3306";
-    $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $porta);
-?>
